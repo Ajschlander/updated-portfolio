245c13f5e8a178d156bb7207a0f959a87f188e1b
Ajschlander <Aschlander21@gmail.com>
2019-12-05 21:35:18 -0600

    Added links so the navbar will scroll to certain sections

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,3 +9,35 @@ VANTA.CELLS({
     size: 5.00,
     speed: 0.00
 })
+
+// Scroll to top when home is clicked
+$("#home-nav-link").click(function () {
+    $('html,body').animate({
+        scrollTop: $(".home-content").offset().top
+    },
+        'slow');
+});
+
+// Scroll to about section when about is clicked
+$("#about-nav-link").click(function () {
+    $('html,body').animate({
+        scrollTop: $(".about-me-container").offset().top
+    },
+        'slow');
+});
+
+// Scroll to portfolio section when portfolio is clicked
+$("#portfolio-nav-link").click(function () {
+    $('html,body').animate({
+        scrollTop: $("").offset().top
+    },
+        'slow');
+});
+
+// Scroll to contact section when contact is clicked
+$("#content-nav-link").click(function () {
+    $('html,body').animate({
+        scrollTop: $("").offset().top
+    },
+        'slow');
+});
